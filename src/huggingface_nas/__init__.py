7b6b6ed930e0003b2a4cf1b5036b30bc338dcec5
tony9402 <tony9402@naver.com>
2025-09-01 13:24:35 +0900

    feat: upload local model to nas

--- src/huggingface_nas/__init__.py
+++ src/huggingface_nas/__init__.py
@@ -1,4 +1,5 @@
 
 from .hf_to_nas import upload_dataset, upload_model
+from .local_to_nas import upload_local_model
 from .load import load_dataset_nas, prepare_model_from_nas
 from .webapi import WebAPI
